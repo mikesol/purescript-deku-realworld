d7b450893cadde89132d9f62d60e9a62dd6d9f2a
Mike Solomon <mike120982@gmail.com>
2022-08-14 22:28:37 +0300

    Adds server to avoid hash problem

--- index.test.js
+++ index.test.js
@@ -1,22 +1,27 @@
-const path = require("path");
-
+let server;
 describe("Conduit", () => {
 	beforeAll(async () => {
-		await page.goto("file://" + path.resolve("dist/index.html"));
+		const app = require("express")();
+		app.use(require("express-static")("dist/"));
+		server = await app.listen(3000);
+		await page.goto("http://localhost:3000");
+	});
+ afterAll(async function () {
+		// Cleanup
+		await server.close();
+ });
+	it('should be titled "Conduit"', async () => {
+		await expect(page.title()).resolves.toMatch("Conduit");
 	});
 
-	// it('should be titled "Conduit"', async () => {
-	// 	await expect(page.title()).resolves.toMatch("Conduit");
-	// });
+	it('should navigate to sign in when clicking on the sign in nav button', async () => {
+	  await page.waitForSelector("#nav-sign-in");
+	  const n = await page.$("#nav-sign-in");
+	  await n.click();
+	  await page.waitForSelector("#sign-in-h1");
+	  const si = await page.$("#sign-in-h1");
+	  const value = await page.evaluate(el => el.textContent, si)
+		expect(value).toMatch("Sign in");
+	});
 
-	// it('should navigate to sign in when clicking on the sign in nav button', async () => {
-  //   // await page.waitForSelector("#nav-sign-in");
-  //   // const n = await page.$("#nav-sign-in");
-  //   // await n.click();
-  //   // await page.waitForSelector("#sign-in-h1");
-  //   // const si = await page.$("#sign-in-h1");
-  //   // const value = await page.evaluate(el => el.textContent, si)
-	// 	// expect(value).toMatch("Sign in");
-	// });
-  it ('tests', () => {expect(1+1).toEqual(2)});
 });
